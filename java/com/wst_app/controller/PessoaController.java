1d21a27b65c413c5e43e1cfb1dab8c02dec3bf93
Anderson Doblinski <anderson.doblinski@portoseguro.com.br>
2020-05-18 22:55:07 -0300

    commit inicial

---
+++ java/com/wst_app/controller/PessoaController.java
@@ -0,0 +1,22 @@
+package com.wst_app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wst_app.common.Pessoa;
+
+@RestController
+public class PessoaController {
+	
+	@RequestMapping(value = "/pessoa", 
+			produces = { "application/json" },
+			method = RequestMethod.GET)
+    public Pessoa Get() {
+		Pessoa pessoa = new Pessoa();
+		pessoa.setNome("Teste");
+        
+		return pessoa;
+    }
+
+}
